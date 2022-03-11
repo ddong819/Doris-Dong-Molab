a50e6463777654ac0b4060c29f4f96339acca864
ddong819 <90161675+ddong819@users.noreply.github.com>
2022-03-11 16:13:47 -0500

    Add files via upload

---
+++ week 6/mycamera/mycamera/contentview.swift
@@ -0,0 +1,21 @@
+////
+////  contentview.swift
+////  mycamera
+////
+////  Created by   dongyunqi on 2022/3/11.
+////
+//
+//import SwiftUI
+//
+//struct ContentView: View{
+//    var body: some View{
+//        GifImage(_name: "1")
+//    }
+//
+//    }
+//
+//struct ContentView_Previews: PreviewProvider{
+//    static var previews: some View{
+//        ContentView()
+//    }
+//}
